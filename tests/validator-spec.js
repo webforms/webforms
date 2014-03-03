0b819f71eebcf65543c734b9d552e7a788bb793d
hotoo <hotoo.cn@gmail.com>
2014-03-03 20:19:22 +0800

    Add test cases

--- tests/validator-spec.js
+++ tests/validator-spec.js
@@ -1,44 +1,118 @@
 define(function(require) {
 
-  var Validator = require('../src/validator');
+  var WebForms2 = require('webforms2');
   var $ = require('$');
   var expect = require('expect');
 
-  describe('validator required', function() {
+  function makeForm(html){
+    var template_form_start = '<form id="form-required" style="display:none;">';
+    var template_form_end = '</form>';
+    return $(template_form_start + html + template_form_end).appendTo(document.body);
+  }
 
-    var template = '<form id="form-required">' +
-      '<input type="text" name="iptText" required />' +
-      '<input type="password" name="iptPwd" required />' +
-      '<input type="checkbox" name="iptCheckbox" required />' +
-      '</form>';
-    var form;
-    var validator;
+  describe('form:validator', function() {
 
-    beforeEach(function(){
-      form = $(template).appendTo(document.body);
-      validator = new Validator(document.getElementById("form-required"));
-    });
-    afterEach(function(){
-      //validator.destory();
-      form && form.remove();
-    });
-
-    it('normal usage', function() {
-      validator.on("fail", function(elem){
-        var name = elem.getAttribute("name");
-        if(name === "iptText" || name === "iptPwd" || name === "iptCheckbox"){
-          expect(true).to.be(true);
-        }else{
-          expect(true).to.be(false);
-        }
+    function testRequiredInvalid(webforms2, data, done){
+      webforms2.on("validate:invalid", function(field){
+        var name = field.name;
+        expect(field.name).to.equal(data.name);
+        done();
       });
-      $("#form-required").submit();
-      validator.off();
+      webforms2.on("validate:valid", function(field){
+        expect(true).to.equal(false);
+        done();
+      });
+    }
 
-      $("#iptText").val("value");
-      $("#form-required").submit();
+    function testRequiredValid(webforms2, data, done){
+      webforms2.on("validate:invald", function(field){
+        expect(true).to.equal(false);
+        done();
+      });
+      webforms2.on("validate:valid", function(field){
+        console.log(1, field)
+        var name = field.name;
+        expect(field.name).to.equal(data.name);
+        done();
+      });
+    }
 
-    });
+    var testCases = [
+      [ 'input[required][value=""]:invald',
+        '<input name="unknow0" value="" required />',
+        testRequiredInvalid,
+        'unknow0'
+      ],
+      [ 'input[required][value=" "]:invald',
+        '<input name="unknow1" value=" " required />',
+        testRequiredInvalid,
+        'unknow1'
+      ],
+      [ 'input[required][value="abc"]:vald',
+        '<input name="unknow2" value="abc" required />',
+        testRequiredValid,
+        'unknow2'
+      ],
+
+      [ 'input[type=text][required][value=""]:invald',
+        '<input type="text" name="text0" value="" required />',
+        testRequiredInvalid,
+        'text0'
+      ],
+      [ 'input[type=text][required][value=" "]:invald',
+        '<input type="text1" name="text1" value=" " required />',
+        testRequiredInvalid,
+        'text1'
+      ],
+      [ 'input[type=text][required][value="abc"]:vald',
+        '<input type="text2" name="text2" value="abc" required />',
+        testRequiredValid,
+        'text2'
+      ],
+
+      [ 'input[type=password][required][value=""]:invald',
+        '<input type="password" name="password0" value="" required />',
+        testRequiredInvalid,
+        'password0'
+      ],
+      [ 'input[type=password][required][value=" "]:invald',
+        '<input type="password" name="password1" value=" " required />',
+        testRequiredInvalid,
+        'password1'
+      ],
+      [ 'input[type=password][required][value="abc"]:vald',
+        '<input type="password" name="password2" value="abc" required />',
+        testRequiredValid,
+        'password2'
+      ]
+    ];
+
+    for(var i=0,l=testCases.length; i<l; i++){
+
+      var desc = testCases[i][0];
+      var elements = testCases[i][1];
+      var handler = testCases[i][2];
+      var name = testCases[i][3];
+
+      (function(desc, elements, handler, name){
+
+        it(desc, function(done) {
+
+          var form = makeForm(elements);
+          var webforms2 = new WebForms2(form);
+
+          handler(webforms2, {name: name}, done);
+
+          webforms2.validate();
+
+          webforms2.off();
+          form.remove();
+
+        });
+
+      })(desc, elements, handler, name);
+    }
+
   });
 
 });
